25ae06f91655c88eb9e5a6c8aa7b7f5ab45b0958
Kamil Senecki <kamil.senecki@deployed.pl>
2023-12-07 15:08:22 +0100

    day7-p2

--- src/aoc/day_07/__init__.py
+++ src/aoc/day_07/__init__.py
@@ -61,7 +61,76 @@ class Challenge(BaseChallenge):
 
     def part_2(self):
         lines = self.input_lines()
-        return
+        hands = []
+        bids = []
+        powers = []
+        for line in lines:
+            hand, bid = line.split(' ')
+            hands.append(hand)
+            bids.append(bid)
+
+        all_hands = list(zip(hands, bids))
+
+        for hand in all_hands:
+            cards_set = set(hand[0])
+            card_counts = dict()
+            for cards in cards_set:
+                count_char = 0
+                for card in hand[0]:
+                    if card == cards:
+                        count_char += 1
+                card_counts[cards] = count_char
+            jokers_count = 0
+            try:
+                jokers_count = card_counts.pop("J")
+            except KeyError:
+                pass
+            max_key = None
+            for key in card_counts:
+                if not max_key:
+                    max_key = key
+                else:
+                    if card_counts[key] > card_counts[max_key]:
+                        max_key = key
+            if jokers_count != 5:
+                card_counts[max_key] += jokers_count
+            else:
+                card_counts["A"] = 5
+
+            if len(set(card_counts.values())) == 1 and 5 in card_counts.values():
+                hand_power = 8
+            elif 4 in card_counts.values():
+                hand_power = 7
+            elif sorted(card_counts.values()) == [2, 3]:
+                hand_power = 6
+            elif 3 in card_counts.values():
+                hand_power = 5
+            elif sorted(card_counts.values()) == [1, 2, 2]:
+                hand_power = 4
+            elif sorted(card_counts.values()) == [1, 1, 1, 2]:
+                hand_power = 3
+            else:
+                hand_power = 2
+
+            powers.append(hand_power)
+
+        hands_with_powers = list(zip(hands, bids, powers))
+
+        key_order = "AKQT98765432J"
+
+        def cards_sort_key(s):
+            return [key_order.index(char) for char in s[0]]
+
+        hands_with_powers.sort(key=cards_sort_key, reverse=True)
+        hands_with_powers.sort(key=lambda a: a[2])
+
+        i = 1
+        total_value = 0
+        for hand in hands_with_powers:
+            total_value = int(total_value) + i * int(hand[1])
+            i += 1
+
+        return total_value
 
 
 if __name__ == "__main__":
