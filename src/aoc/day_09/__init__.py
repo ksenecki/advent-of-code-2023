ad46471e7668021524fbec382a77a44d85f5ef82
Kamil Senecki <kamil.senecki@deployed.pl>
2023-12-09 12:42:18 +0100

    day9-py2

--- src/aoc/day_09/__init__.py
+++ src/aoc/day_09/__init__.py
@@ -26,17 +26,17 @@ class Challenge(BaseChallenge):
         extrapolated_values = []
         for line in lines:
             sequences = [list(map(int, line.split()))]
+            sequences[0].reverse()
+            while any(sequences[-1]):
+                next_sequence = [sequences[-1][i + 1] - sequences[-1][i] for i in range(len(sequences[-1]) - 1)]
+                sequences.append(next_sequence)
 
-            while any(sequences[0]):
-                prev_sequence = [sequences[0][i] - sequences[0][i - 1] for i in range(1, len(sequences[0]))]
-                sequences.insert(0, prev_sequence)
-
-            for i in range(1, len(sequences)):
-                sequences[i] = [sequences[i][j] + (sequences[i][j - 1] if j > 0 else 0) for j in
+            for i in range(len(sequences) - 2, -1, -1):
+                sequences[i] = [sequences[i][j] + (sequences[i + 1][j - 1] if j > 0 else 0) for j in
                                 range(len(sequences[i]))]
 
-            extrapolated_values.append(sequences[-1][-1])
-        # does not work :(
+            extrapolated_values.append(sequences[0][-1])
+
         return sum(extrapolated_values)
 
 
--- src/aoc/day_09/test_solution.py
+++ src/aoc/day_09/test_solution.py
@@ -6,4 +6,4 @@ from . import Challenge
 class TestChallenge(BaseTestChallenge):
     challenge_class = Challenge
     expected_results_from_test_data = (114, 2)
-    expected_results_from_real_data = (1938731307, NotSet)
+    expected_results_from_real_data = (1938731307, 948)
